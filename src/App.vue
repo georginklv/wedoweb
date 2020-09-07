599f07db2d8f59a7f26deba7008f9536ccd0f9ec
georginklv <georginklv@abv.bg>
2020-09-07 14:25:13 +0300

    delete style files and set fonts

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,48 @@
 		<TheFooter />
 	</v-app>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "~vuetify/src/styles/settings/_variables.scss";
+@font-face {
+	font-family: "Phenomena-Regular";
+	src: url("../public/fonts/Phenomena-Regular.woff2");
+}
+$font-size-root: 20px !important;
+$body-font-family: "Phenomena-Regular" !important;
+html,
+body,
+.v-application {
+	font-family: $body-font-family;
+}
+.v-application {
+	.display-4,
+	.display-3,
+	.display-2,
+	.display-1,
+	.headline,
+	.title,
+	.subtitle-1,
+	.subtitle-2,
+	.body-1,
+	.body-2,
+	.caption,
+	.overline {
+		font-family: $body-font-family;
+	}
+}
+.wrapper {
+	display: flex;
+	flex-direction: column;
+	max-width: 1600px;
+}
+</style>
 <script>
-import TheHeader from './components/TheHeader';
-import TheNavBar from './components/TheNavBar';
-import TheFooter from './components/TheFooter';
+import TheHeader from "./components/TheHeader";
+import TheNavBar from "./components/TheNavBar";
+import TheFooter from "./components/TheFooter";
 
 export default {
-	name: 'App',
+	name: "App",
 
 	components: {
 		TheHeader,
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
 	lintOnSave: false,
 	transpileDependencies: ["vuetify"],
-	css: {
-		loaderOptions: {
-			sass: {
-				data: `@import "@/styles/_variables.scss";`,
-			},
-		},
-	},
 };
