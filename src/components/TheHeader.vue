4dfaf23da954b26b424277c32a7faaba592297c6
georginklv <georginklv@abv.bg>
2020-09-27 23:34:05 +0300

    add fluid obj

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,6 +2,14 @@
 	<v-app>
 		<div class="wrapper">
 			<v-col cols="auto" class="pa-0">
+				<div class="wrap">
+					<div class="shape"></div>
+					<div class="shape"></div>
+					<div class="shape"></div>
+					<div class="shape"></div>
+					<div class="shape"></div>
+					<div class="shape"></div>
+				</div>
 				<img src="../assets/headerPic.png" class="headerImg" alt="header-image" />
 				<v-row class="btn-wrapper">
 					<div class="line"></div>
@@ -113,14 +121,14 @@ export default {
 		menu: false,
 		dialog: false,
 		items: [
-			'Web Design & Development',
-			'Corporate Identity',
-			'Web Site Re-design',
-			'SEO Optimization',
-			'UX/UI',
-			'Online Shop',
-			'Social Media Marketing',
-			'Other',
+			"Web Design & Development",
+			"Corporate Identity",
+			"Web Site Re-design",
+			"SEO Optimization",
+			"UX/UI",
+			"Online Shop",
+			"Social Media Marketing",
+			"Other",
 		],
 	}),
 };
@@ -134,9 +142,91 @@ export default {
 	position: relative;
 	display: flex;
 	margin: 0 auto;
+	max-width: 1300px;
 }
 .line {
 	width: 500px;
+}
+.wrap {
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+}
+.shape:nth-of-type(1) {
+	width: 1270px;
+	height: 630px;
+	top: -50px;
+	position: absolute;
+	background: linear-gradient(64deg, #f34868 23%, #f24768 23%, #9e00ec 79%);
+	animation: wave 8s ease-in-out infinite;
+}
+.shape:nth-of-type(2) {
+	width: 800px;
+	height: 800px;
+	position: absolute;
+	top: -200px;
+	left: -400px;
+	opacity: 0.2;
+	z-index: -1;
+	background-color: #7998ff;
+	animation: wave 5s ease-in-out infinite;
+}
+.shape:nth-of-type(3) {
+	width: 500px;
+	height: 500px;
+	position: absolute;
+	bottom: -25%;
+	right: -5%;
+	opacity: 0.3;
+	z-index: -1;
+	background-color: #379eff;
+	animation: wave 12s ease-in-out infinite;
+}
+.shape:nth-of-type(4) {
+	width: 250px;
+	height: 250px;
+	position: absolute;
+	bottom: -120px;
+	left: 20%;
+	opacity: 0.4;
+	z-index: -1;
+	background-color: #fbff0d;
+	animation: wave 6s ease-in-out infinite;
+}
+.shape:nth-of-type(5) {
+	width: 350px;
+	height: 350px;
+	position: absolute;
+	top: -8px;
+	left: 10%;
+	opacity: 0.2;
+	z-index: -1;
+	background-color: #ff4343;
+	animation: wave 15s ease-in-out infinite;
+}
+.shape:nth-of-type(6) {
+	width: 100px;
+	height: 100px;
+	position: absolute;
+	top: 40px;
+	left: 25%;
+	transform: rotate(90deg);
+	opacity: 0.5;
+	z-index: -1;
+	background-color: #ff4343;
+	animation: wave 10s ease-in-out infinite;
+}
+@keyframes wave {
+	0%,
+	100% {
+		border-radius: 66% 34% 37% 63% / 57% 31% 69% 43%;
+	}
+	50% {
+		border-radius: 26% 74% 51% 49% / 22% 53% 47% 78%;
+	}
 }
 .v-btn {
 	font-size: 20px !important;
