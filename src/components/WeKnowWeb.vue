e68ec2b058640c42f11f470f2e965183a6ece508
georginklv <georginklv@abv.bg>
2020-10-25 20:20:27 +0200

    bump

--- src/components/WeKnowWeb.vue
+++ src/components/WeKnowWeb.vue
@@ -16,21 +16,7 @@
 					<v-col class="boxes" cols="auto" md="4" sm="6">
 						<v-card class="mx-auto " max-width="400" elevation="0">
 							<v-row justify="center">
-								<v-list-item-avatar tile size="50"
-									><svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 55 55"
-										fill="currentColor"
-										class="_2B4XG7yxo9svt1jTWVKRhj _2BVHUMSkfvQym-c2jIUgBq"
-									>
-										<path
-											d="M22.019 25.877a1.004 1.004 0 001.018-.033l11-7a1.002 1.002 0 000-1.688l-11-7a1.005 1.005 0 00-1.019-.033A1.002 1.002 0 0021.5 11v14c0 .365.199.701.519.877zM23.5 12.821L31.638 18 23.5 23.179V12.821z"
-										></path>
-										<path d="M.5 0v55h54V0H.5zm52 2v33h-50V2h50zm-50 51V37h50v16h-50z"></path>
-										<path
-											d="M47.5 44h-24v-2a1 1 0 10-2 0v2h-4a1 1 0 100 2h4v2a1 1 0 102 0v-2h24a1 1 0 100-2zM7.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1zM11.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1z"
-										></path></svg
-								></v-list-item-avatar>
+								<v-list-item-avatar tile size="90"> <img src="../assets/web.png" alt="" /> </v-list-item-avatar>
 							</v-row>
 							<v-list-item-title class="headline mb-1 pa-4">Web design & Development</v-list-item-title>
 
@@ -50,21 +36,7 @@
 					<v-col class="boxes" cols="auto" md="4" sm="6">
 						<v-card class="mx-auto " max-width="400" elevation="0">
 							<v-row justify="center">
-								<v-list-item-avatar tile size="50"
-									><svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 55 55"
-										fill="currentColor"
-										class="_2B4XG7yxo9svt1jTWVKRhj _2BVHUMSkfvQym-c2jIUgBq"
-									>
-										<path
-											d="M22.019 25.877a1.004 1.004 0 001.018-.033l11-7a1.002 1.002 0 000-1.688l-11-7a1.005 1.005 0 00-1.019-.033A1.002 1.002 0 0021.5 11v14c0 .365.199.701.519.877zM23.5 12.821L31.638 18 23.5 23.179V12.821z"
-										></path>
-										<path d="M.5 0v55h54V0H.5zm52 2v33h-50V2h50zm-50 51V37h50v16h-50z"></path>
-										<path
-											d="M47.5 44h-24v-2a1 1 0 10-2 0v2h-4a1 1 0 100 2h4v2a1 1 0 102 0v-2h24a1 1 0 100-2zM7.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1zM11.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1z"
-										></path></svg
-								></v-list-item-avatar>
+								<v-list-item-avatar tile size="90"><img src="../assets/corporate.png" alt="" /> </v-list-item-avatar>
 							</v-row>
 							<v-list-item-title class="headline mb-1 pa-4">Corporate Identity</v-list-item-title>
 
@@ -84,21 +56,7 @@
 					<v-col class="boxes" cols="auto" md="4" sm="6">
 						<v-card class="mx-auto " max-width="400" elevation="0" disabled>
 							<v-row justify="center">
-								<v-list-item-avatar tile size="50"
-									><svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 55 55"
-										fill="currentColor"
-										class="_2B4XG7yxo9svt1jTWVKRhj _2BVHUMSkfvQym-c2jIUgBq"
-									>
-										<path
-											d="M22.019 25.877a1.004 1.004 0 001.018-.033l11-7a1.002 1.002 0 000-1.688l-11-7a1.005 1.005 0 00-1.019-.033A1.002 1.002 0 0021.5 11v14c0 .365.199.701.519.877zM23.5 12.821L31.638 18 23.5 23.179V12.821z"
-										></path>
-										<path d="M.5 0v55h54V0H.5zm52 2v33h-50V2h50zm-50 51V37h50v16h-50z"></path>
-										<path
-											d="M47.5 44h-24v-2a1 1 0 10-2 0v2h-4a1 1 0 100 2h4v2a1 1 0 102 0v-2h24a1 1 0 100-2zM7.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1zM11.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1z"
-										></path></svg
-								></v-list-item-avatar>
+								<v-list-item-avatar tile size="90"><img src="../assets/shop.png" alt=""/></v-list-item-avatar>
 							</v-row>
 							<v-list-item-title class="headline mb-1 pa-4">Online Shop</v-list-item-title>
 
@@ -118,21 +76,7 @@
 					<v-col class="boxes" cols="auto" md="4" sm="6">
 						<v-card class="mx-auto " max-width="400" elevation="0">
 							<v-row justify="center">
-								<v-list-item-avatar tile size="50"
-									><svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 55 55"
-										fill="currentColor"
-										class="_2B4XG7yxo9svt1jTWVKRhj _2BVHUMSkfvQym-c2jIUgBq"
-									>
-										<path
-											d="M22.019 25.877a1.004 1.004 0 001.018-.033l11-7a1.002 1.002 0 000-1.688l-11-7a1.005 1.005 0 00-1.019-.033A1.002 1.002 0 0021.5 11v14c0 .365.199.701.519.877zM23.5 12.821L31.638 18 23.5 23.179V12.821z"
-										></path>
-										<path d="M.5 0v55h54V0H.5zm52 2v33h-50V2h50zm-50 51V37h50v16h-50z"></path>
-										<path
-											d="M47.5 44h-24v-2a1 1 0 10-2 0v2h-4a1 1 0 100 2h4v2a1 1 0 102 0v-2h24a1 1 0 100-2zM7.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1zM11.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1z"
-										></path></svg
-								></v-list-item-avatar>
+								<v-list-item-avatar tile size="90"><img src="../assets/redesign.png" alt=""/></v-list-item-avatar>
 							</v-row>
 							<v-list-item-title class="headline mb-1 pa-4">Website Re-design</v-list-item-title>
 
@@ -152,21 +96,7 @@
 					<v-col class="boxes" cols="auto" md="4" sm="6">
 						<v-card class="mx-auto " max-width="400" elevation="0">
 							<v-row justify="center">
-								<v-list-item-avatar tile size="50"
-									><svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 55 55"
-										fill="currentColor"
-										class="_2B4XG7yxo9svt1jTWVKRhj _2BVHUMSkfvQym-c2jIUgBq"
-									>
-										<path
-											d="M22.019 25.877a1.004 1.004 0 001.018-.033l11-7a1.002 1.002 0 000-1.688l-11-7a1.005 1.005 0 00-1.019-.033A1.002 1.002 0 0021.5 11v14c0 .365.199.701.519.877zM23.5 12.821L31.638 18 23.5 23.179V12.821z"
-										></path>
-										<path d="M.5 0v55h54V0H.5zm52 2v33h-50V2h50zm-50 51V37h50v16h-50z"></path>
-										<path
-											d="M47.5 44h-24v-2a1 1 0 10-2 0v2h-4a1 1 0 100 2h4v2a1 1 0 102 0v-2h24a1 1 0 100-2zM7.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1zM11.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1z"
-										></path></svg
-								></v-list-item-avatar>
+								<v-list-item-avatar tile size="90"><img src="../assets/seo.png" alt=""/></v-list-item-avatar>
 							</v-row>
 							<v-list-item-title class="headline mb-1 pa-4">SEO Optimization</v-list-item-title>
 
@@ -186,21 +116,7 @@
 					<v-col class="boxes" cols="auto" md="4" sm="6">
 						<v-card class="mx-auto " max-width="400" elevation="0">
 							<v-row justify="center">
-								<v-list-item-avatar tile size="50"
-									><svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 55 55"
-										fill="currentColor"
-										class="_2B4XG7yxo9svt1jTWVKRhj _2BVHUMSkfvQym-c2jIUgBq"
-									>
-										<path
-											d="M22.019 25.877a1.004 1.004 0 001.018-.033l11-7a1.002 1.002 0 000-1.688l-11-7a1.005 1.005 0 00-1.019-.033A1.002 1.002 0 0021.5 11v14c0 .365.199.701.519.877zM23.5 12.821L31.638 18 23.5 23.179V12.821z"
-										></path>
-										<path d="M.5 0v55h54V0H.5zm52 2v33h-50V2h50zm-50 51V37h50v16h-50z"></path>
-										<path
-											d="M47.5 44h-24v-2a1 1 0 10-2 0v2h-4a1 1 0 100 2h4v2a1 1 0 102 0v-2h24a1 1 0 100-2zM7.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1zM11.5 41a1 1 0 00-1 1v6a1 1 0 102 0v-6a1 1 0 00-1-1z"
-										></path></svg
-								></v-list-item-avatar>
+								<v-list-item-avatar tile size="90"><img src="../assets/ux.png" alt=""/></v-list-item-avatar>
 							</v-row>
 							<v-list-item-title class="headline mb-1 pa-4">UX/UI</v-list-item-title>
 
@@ -241,7 +157,6 @@ export default {};
 		box-shadow: 0 0 35px -2px rgba(0, 0, 0, 0.2) !important;
 		.v-list-item__avatar {
 			transition: color 0.3s, transform 0.3s ease-out;
-			color: #626ee3 !important;
 			transform: scale(1.1);
 		}
 	}
