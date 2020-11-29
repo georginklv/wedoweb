725d5cda976367701fa65e5dff49eee7f958669c
Georgi <g.nikolov@next-it.bg>
2020-11-29 18:44:29 +0200

    Implement PWA

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+const { GenerateSW } = require('workbox-webpack-plugin');
 module.exports = {
 	lintOnSave: false,
-	transpileDependencies: ["vuetify"],
+	transpileDependencies: ['vuetify'],
+	publicPath: process.env.NODE_ENV === 'development' ? '/vuejs-pwa/' : '',
+
+	configureWebpack: {
+		plugins: [new GenerateSW()],
+	},
 };
