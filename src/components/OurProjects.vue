879fc430f905c95f999a812f5b54db81dc003c4c
Diyana Ruseva <diyana.rrr@gmail.com>
2020-12-28 17:27:32 +0200

    * change header, remove video, add responsive sizes

--- src/components/OurProjects.vue
+++ src/components/OurProjects.vue
@@ -1,76 +1,92 @@
 <template>
-	<v-app>
-		<div class="wrapper mt-16">
-			<v-col class="pa-0">
-				<div class="title">
-					<p class="font-weight-medium text-uppercase">Our</p>
-					<p class="font-weight-black text-uppercase">Projects</p>
-					<div class="line"></div>
-				</div>
-				<v-row class="card-wrapper my-10">
-					<vue-video-section
-						:elementId="'header-background-video'"
-						:ref="'header-background-video'"
-						:mp4Source="require('../assets/reversedG5.mp4')"
-						:mobileBreakpoint="992"
-						:desktopHeight="550"
-						:mobileHeight="450"
-						:playsinline="true"
-						:loop="true"
-						:autoplay="true"
-						:autobuffer="true"
-						:muted="true"
-					>
-					</vue-video-section>
-				</v-row>
-				<v-row class="card-wrapper my-10">
-					<v-btn to="/portfolio" color="orange" class="rounded-pill" text>
-						portfolio
-					</v-btn>
-				</v-row>
-			</v-col>
-		</div>
-	</v-app>
+  <v-app>
+    <div class="wrapper mt-16">
+      <v-col class="pa-0">
+        <div class="title">
+          <p class="font-weight-medium text-uppercase">Our</p>
+          <p class="font-weight-black text-uppercase">Projects</p>
+          <div class="line"></div>
+        </div>
+        <v-row class="card-wrapper my-10">
+          <div class="video-scroll">
+            <img src="../assets/mockUpWide.png" class="mock-up" />
+            <div class="img-box"></div>
+          </div>
+        </v-row>
+        <v-row class="card-wrapper my-10">
+          <v-btn to="/portfolio" color="orange" class="rounded-pill" text>
+            portfolio
+          </v-btn>
+        </v-row>
+      </v-col>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import 'vue-video-section/dist/vue-video-section.css';
-import VueVideoSection from 'vue-video-section';
+// import "vue-video-section/dist/vue-video-section.css";
+// import VueVideoSection from "vue-video-section";
 export default {
-	components: {
-		VueVideoSection,
-	},
+  components: {
+    // VueVideoSection,
+  },
 
-	data: () => ({
-		//
-	}),
+  data: () => ({
+    //
+  }),
 };
 </script>
 
 <style lang="scss" scoped>
 .row {
-	display: flex;
-	flex-wrap: wrap !important;
-	flex: 0 1 auto;
-	justify-content: space-around;
+  display: flex;
+  flex-wrap: wrap !important;
+  flex: 0 1 auto;
+  justify-content: space-around;
+}
+.video-scroll {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  box-shadow: 0 0 40px 0 #00000040;
+  margin: 0 auto;
+}
+.img-box {
+  z-index: 100;
+  width: 666px;
+  height: 389px;
+  background: black;
+  transition: ease-in-out 10s;
+  background: url(../assets/georginklvSite.png);
+  background-size: cover;
+  background-position: top;
+  position: absolute;
+  transform: translate(238%, 131px);
+  cursor: pointer;
+}
+.img-box:hover {
+  background-position: bottom;
+}
+.mock-up {
+  z-index: 0;
 }
 .v-btn {
-	background-color: #ff4f40;
-	padding: 10px 50px !important;
-	color: #fff !important;
-	&:active,
-	&:focus,
-	&:hover {
-		color: #fff !important;
-		background-color: #301aff !important;
-	}
+  background-color: #ff4f40;
+  padding: 10px 50px !important;
+  color: #fff !important;
+  &:active,
+  &:focus,
+  &:hover {
+    color: #fff !important;
+    background-color: #301aff !important;
+  }
 }
 .vue-video-section__overlay-content-wrapper__background {
-	background: unset !important;
-	opacity: 1 !important;
-	border: none !important;
+  background: unset !important;
+  opacity: 1 !important;
+  border: none !important;
 }
 .vue-video-section-wrapper {
-	background: unset !important;
+  background: unset !important;
 }
 </style>
