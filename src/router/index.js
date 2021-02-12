2825b89fad20734d28fc86e611986647679ec4b2
Georgi <g.nikolov@next-it.bg>
2021-02-12 09:51:30 +0200

    router changes

--- src/router/index.js
+++ src/router/index.js
@@ -4,51 +4,49 @@ import storeBlogs from '@/data/storeBlogs';
 
 Vue.use(VueRouter);
 
-const routes = [
-	{
-		path: '/',
-		name: 'Home',
-		component: require('../views/Home.vue').default,
-	},
-	{
-		path: '/portfolio',
-		name: 'Portfolio',
-		component: require('../views/Portfolio.vue').default,
-	},
-	{
-		path: '/blog',
-		name: 'Blog',
-		component: require('../views/allBlogs.vue').default,
-	},
-	{
-		path: '/blog/:slug',
-		name: 'BlogDetails',
-		props: true,
-		component: () =>
-			import(
-				/* webpackChunkName: "DestinationDetails"*/ '../views/BlogPage.vue'
-			),
-		beforeEnter: (to, from, next) => {
-			const exists = storeBlogs.blogs.find(
-				(blog) => blog.slug === to.params.slug,
-			);
-			if (exists) {
-				next();
-			} else {
-				next({ name: 'notFound' });
-			}
-		},
-	},
-	{
-		path: '/contacts',
-		name: 'Contacts',
-		component: require('../views/Contacts.vue').default,
-	},
-];
-
 const router = new VueRouter({
 	mode: 'history',
-	routes,
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: require('../views/Home.vue').default,
+		},
+		{
+			path: '/portfolio',
+			name: 'Portfolio',
+			component: require('../views/Portfolio.vue').default,
+		},
+		{
+			path: '/blog',
+			name: 'Blog',
+			component: require('../views/allBlogs.vue').default,
+		},
+		{
+			path: '/blog/:slug',
+			name: 'BlogDetails',
+			props: true,
+			component: () =>
+				import(
+					/* webpackChunkName: "DestinationDetails"*/ '../views/BlogPage.vue'
+				),
+			beforeEnter: (to, from, next) => {
+				const exists = storeBlogs.blogs.find(
+					(blog) => blog.slug === to.params.slug,
+				);
+				if (exists) {
+					next();
+				} else {
+					next({ name: 'notFound' });
+				}
+			},
+		},
+		{
+			path: '/contacts',
+			name: 'Contacts',
+			component: require('../views/Contacts.vue').default,
+		},
+	],
 });
 
 export default router;
